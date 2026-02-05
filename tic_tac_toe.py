73915771c54da855a4a01918d11d7cd27eb3b661
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-02-05 17:44:42 +0545

    complete tic-toe game

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -11,4 +11,30 @@ def check_winner():
            buttons[combo[2]].config(bg="green")
            winner = buttons[combo[0]]['text']
            messagebox.showinfo("Tic-Tac-Toe", f"Player {winner} wins!")
-           return winner
+           root.quit()   # close the game window
+
+def button_click(index):
+    if buttons[index]["text"]=="" and not winner:
+         buttons[index]["text"]=current_player
+         check_winner()
+         toggle_player()
+    
+def toggle_player():
+    global current_player
+    current_player = "x" if current_player =="0" else "0"
+    lable.config(text=f" player {current_player} 's turn")
+
+root=tk.Tk()  # create the main window
+root.title("tic_tac_toe")  # 
+
+buttons=[tk.Button(root,text="",font=('"normal',25),width=6 , height=3,command=lambda i=i: button_click(i)) for i in range(9)]
+    
+for i, button in enumerate(buttons):
+    button.grid(row=i//3, column=i%3)    # arrange the buttons in a 3x3 grid
+
+current_player="x"    
+winner=False   # to keep track of the winner
+lable=tk.Label(root,text=f"player {current_player} 's turn", font=("normal",20))     
+lable.grid(row=3, column=0, columnspan=3)  # display the current player's turn
+
+root.mainloop()  # start the GUI event loop
