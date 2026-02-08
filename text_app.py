acc2d612d02a3782f6489eefaecd5bd199d78ceb
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-02-08 23:02:37 +0545

    make more reliable to text editor app

--- text_app.py
+++ text_app.py
@@ -1,4 +1,4 @@
-import tkinteras as tk
+import tkinter as tk
 from tkinter import filedialog , messagebox
 
 def new_file():
@@ -17,7 +17,37 @@ def save_file():
     filepath=filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Documents","*.txt")])
     if filepath:
         with open(filepath, 'w') as file:
-            file.write(text.get(1.0, tk.END))
+            file.write(text.get(1.0, tk.END))   # write the content of the text area to the file
             messagebox.showinfo("save file", "file saved successfully")
+            
+def exit_App():
+    if messagebox.askyesno("Exit", "Do you  want to exit"):
+        root.destroy()  # close the application
 
-        
+
+def dark_mode():
+    text.config(bg="black", fg="white",insertbackground="white")
+
+def light_mode():
+    text.config(bg="white", fg="black", insertbackground="black")
+
+root=tk.Tk()
+root.title("Simple Text Editor")
+root.geometry("600x400")
+
+menu_bar=tk.Menu(root)
+file_menu=tk.Menu(menu_bar, tearoff=0)  # create a file menu and add options to it
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_command(label="Save As", command=save_file)
+menu_bar.add_cascade(label="File", menu=file_menu)
+root.config(menu=menu_bar)
+
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=exit_App)    # add exit option to file menu the text area
+
+text=tk.Text(root, wrap=tk.WORD, undo=True)
+text.pack(expand=True, fill='both')
+
+root.mainloop()
