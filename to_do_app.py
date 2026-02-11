c36591d67a041b923cf1b611694a27cf44fc6353
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-02-11 21:51:03 +0545

    make a two operation of to do list app

---
+++ to_do_app.py
@@ -0,0 +1,27 @@
+
+def task():
+    tasks= []  # empty list
+    print(" ==========Welcome to the task management app=====")
+    
+
+total_task=int(input("enter how many task you want to add="))   #5
+for i in range(1,total_task+1):
+    task_name=input("enter task{i}=") # enter task 3
+    tasks.append(task_name)
+
+print(f"Todays tasks are \n {tasks}")
+
+while True:
+    operation=int(input("enter 1- Add \n2-Update\n3 -delete\n4-view\n5 -Exit/stop"))
+    if operation==1:
+        add=input("enter the task  you want to add :")
+        tasks.append(add)
+        print("task{add} has been sucessfully added ....")
+
+    elif operation==2:
+        update_val=input("enter the task name you want to update=")
+        if update_val in tasks:
+            up=input("enter new tasks=")
+            ind=tasks.index(update_val)
+            tasks[ind]=up
+            print(f"updated task{up}")
