286b1e8af014fbaf39ea852a7c6c4a9951f78cf9
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-02-15 10:28:55 +0545

    mainitained the all operation of the student grade management system

--- Stu_mana_sys.py
+++ Stu_mana_sys.py
@@ -22,3 +22,42 @@ def delete_student(name):
         print(f"{name} has been sucessfully deleted:")
     else:
         print(f"{name} is not found!")
+
+
+#  view all student
+def display_all_student():
+    if student_grades():
+        for name , grade in student_grades.items():
+            print(f"{name} : {grade}")
+    else:
+        print("no student  found/added")
+
+
+#   mian function
+def main():
+    while True:
+        print('\nStudent grades management system: ')
+        print("2. Add student")
+        print("2. Update Student : ")
+        print("3.Delete student:  ")
+        print("4. View Student: ")
+        print("5. Exit")
+
+        choice=int(input("enter your choice="))
+        if choice==1:
+            name=input("enter student name==")
+            grade=int(input("enter student grade="))
+            add_student(name,grade)
+        elif choice==2:
+            name=input("enter student name= ")
+            grade=int(input("enter student grade="))
+            update_student(name,grade)
+        elif choice==3:
+            name=input("enter student name=")
+            delete_student(name)
+        elif choice==4:
+            display_all_student()
+        elif choice==5:
+            print("closing the program .... ")
+        else:
+            print("invilid choice")
