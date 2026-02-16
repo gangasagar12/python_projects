0190ba4d03f508c8cb29a7224592bf82d3cc57af
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-02-16 14:41:58 +0545

    manage small opetation of file

--- file manage/app.py
+++ file manage/app.py
@@ -36,3 +36,60 @@ def read_file(filename):
     
     except FileNotFoundError:
         print("filename doestnot exits")
+
+    except exception as e:
+        print('an error occured')
+
+def edit_file(filename):
+    try:
+        with open('sample.txt' ,'a') as f:
+            content=input("enter data to add=")
+            f.write(content+ "\n")
+            print("content addded to {filename} sucessfully !")
+    except FileNotFoundError:
+        print(f"{filename} does not exits")
+
+    except exception as e:
+        print("an error occured")
+
+def main():
+    while True:
+        print('file  management app')
+        print("1: create a file")
+        print("2: view all files")
+        print("3. delete files")
+        print("4. read file")
+        print("5: edit files")
+        print("6: exit")
+
+        choice=input("enter your choice(1-6):")
+        if choice=="1":
+            filename=input("enter the filename to creeate=")
+            create_file(filename)
+
+        elif choice=="2":
+            view_all_files()
+
+        elif choice=="3":
+            filename=input("enter the name of file you want to delete=")
+            deletefile(filename)
+        
+        elif choice=="4":
+            filename=input("enter file name to read=")
+            read_file(filename)
+        elif choice=="5":
+            filename=input("enter file name to edit=")
+            edit_file(filename)
+        elif choice=="6":
+            print("closing the app...")
+            break
+
+        else:
+            print("invalid syntax")
+
+
+if __name__=="__main__":
+    main()
+
+
+
