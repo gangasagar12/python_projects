3dd9ec0849f4fb7fe2519ead001b894636d38000
gangasagar12 <ganga15bca2024@kcc.edu.np>
2026-02-20 22:59:41 +0545

    manage contact function

--- file manage/contact_app.py
+++ file manage/contact_app.py
@@ -1,4 +1,4 @@
-contact={}
+contacts={}
  
 while True:
     print("\n contact book  app")
@@ -12,7 +12,7 @@ while True:
 
     choice=input("enter your choice=")
 
-    if choice==1:
+    if choice=="1":
         name=input("enter contact name=")
         if name in contacts:
             print(f"contact name {name} already exits")
@@ -24,10 +24,49 @@ while True:
             contacts[name]={"age":age,"phone":phone,"mobile":mobile,"email":email}
             print(f"contact {name} created successfully")
 
-    elif choice==2:
+    elif choice=="2":
         name=input("enter contact name to view=")
         if name in contacts:
             contact=contacts[name]
             print(f"Name: {name},Age: {contact['age']}, Mobile: {contact['mobile']}, Email: {contact['email']}")
         else:
             print(f"contact not found with the name { name}")
+
+    elif choice=='4':
+        name=input("enter name to update contact=")
+        if name in contacts:
+            age=input("enter new age=")
+            phone=input("enter new phone number=")
+            mobile=input("enter new mobile number=")
+            email=input("enter new email address=")
+            contacts[name]={"age":age,"phone":phone,"mobile":mobile,"email":email}
+        else:
+            print(f"contact not found with the name {name}")
+
+    elif choice=='3':
+        name=input("enter name to delete contact=")
+        if name in contacts:
+            del contacts[name]
+            print(f"contact {name} deleted successfully")
+        else:
+            print(f"contact not found with the name {name}")
+
+    elif choice=='5':
+        name=input("enter name to search contact=")
+        found=False
+        for name, contact in contacts.items():
+            if name.lower() in name.lower():
+                print(f" found Name: {name}, Age: {contact['age']}, Mobile: {contact['mobile']}, Email: {contact['email']}")
+                found=True
+
+        if not found:
+            print(f"contact not found with the name {name}")   
+    
+    elif choice=='6':
+        print(f"total contacts: {len(contacts)}")
+    elif choice=="7":
+        print("exiting contact book app")
+        break
+    
+    else:
+        print("invalid choice. please try again.")
